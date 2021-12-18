e6750454569b9ca2c5012a33179eafc23157878e
Andrey Golikov <z-a-t-o@yandex.ru>
2021-12-18 15:51:22 +0300

    flask db init создает каталог миграций в проекте

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
 #для миграций б.д.
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
+from flask_migrate import Migrate
+
 
 
 app = Flask(__name__)
@@ -12,6 +12,5 @@ data_base = SQLAlchemy(app)
 
 #создал объект класса Migrate в качестве параметров app и data_base для их корреяции
 migrate = Migrate(app, data_base)
-manager = Manager(app)
-manager.add_command('data_base', MigrateCommand)
+
 
