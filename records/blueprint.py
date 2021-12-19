9c7a2f38c6b72802e60d2d35a3ad6003e5a8de35
Andrey Golikov <z-a-t-o@yandex.ru>
2021-12-19 13:32:34 +0300

    добавлен поиск по имени компании или по содержимому кратких отчётов

--- records/blueprint.py
+++ records/blueprint.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import render_template
 from models import Records
+from flask import request
 
 
 
@@ -11,9 +12,17 @@ records = Blueprint('records', __name__, template_folder='templates')
 
 
 # страница представления таблицы
-@records.route('/')
+@records.route('/', methods = ['POST', 'GET'])
 def index():
-    records = Records.query.all()
+    # в переменную search считывается то, что пользователь ввёл в строку поиска
+    # далее условие: если переменная search содержит что - либо, тогда у класса Records - модели запросов в б.д.
+    # запрашиваем данные по сравниванию пользовательского запроса с именами компаний или содержанию краткого отчёта
+    search = request.args.get('search')
+    if search:
+        records = Records.query.filter(Records.company_name.contains(search) | Records.short_info.contains(search)).all()
+    # иначе рендерится страница со всеми записями
+    else:
+        records = Records.query.all()
     return render_template('records/index.html', records=records)
 
 # поиск по слагу в б.д. <...> - это переменная динамической ссылки
@@ -22,6 +31,5 @@ def record_detail(slug):
     # query.filter может выдать запрос списком если нашлось несколько результатов,
     # так как слаг уникальный, то берём первый попавшийся результат ( метод .first() )
     record = Records.query.filter(Records.slug == slug).first()
-    print(record)
     # возвращается страница с детальной информацией о компании
     return render_template('records/record_detail.html', record=record)
