06c1eaf80cc6b6e0fd6a5e2a74ffe73c8bbaa92a
Andrey Golikov <z-a-t-o@yandex.ru>
2021-12-28 22:09:04 +0300

    секретный ключ

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ class Configuration(object):
     #Отключим чтобы не влиял на производительность
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://ujkbzy:jVr8gf3BkBDb2k2I@localhost/bot_db'
+    # без ключа некоторый функционал может быть недоступен, например объект сессии(куки серв-клиент)
     SECRET_KEY = 'Hdf72Lkfj872ZWq83Lru32imn'
