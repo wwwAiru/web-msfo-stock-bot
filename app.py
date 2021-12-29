df963e33d6859aec3fb4c35dae0368ec33b246b7
Andrey Golikov <z-a-t-o@yandex.ru>
2021-12-29 21:28:05 +0300

    локализация админки

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
+from flask_babelex import Babel
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 data_base = SQLAlchemy(app)
-
+babel = Babel(app)
 # создал объект класса Migrate в качестве параметров app и data_base для их корреяции
 migrate = Migrate(app, data_base)
 
@@ -20,4 +22,11 @@ from models import *
 # создан экземпляр класса Admin
 admin = Admin(app)
 # таким образом можно добавить управление любыми данными
-admin.add_view(ModelView(Records, data_base.session))
+admin.add_view(ModelView(Records, data_base.session, name='Таблица МСФО'))
+
+# локализация админки
+@babel.localeselector
+def get_locale():
+        # Put your logic here. Application can store locale in
+        # user profile, cookie, session, etc.
+        return 'ru'
