5c2dcad892cccc2ce03133d4af0be9ece3ff3681
Andrey Golikov <z-a-t-o@yandex.ru>
2022-01-04 17:13:32 +0300

    flask-security соль и хэш для паролей

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ class Configuration(object):
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://ujkbzy:jVr8gf3BkBDb2k2I@localhost/bot_db'
     # без ключа некоторый функционал может быть недоступен, например объект сессии(куки серв-клиент)
     SECRET_KEY = 'Hdf72Lkfj872ZWq83Lru32imn'
+    # flask-security соль и хэш для паролей
+    SECURITY_PASSWORD_SALT = 'f3BkBDa8k4Wz9h'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
