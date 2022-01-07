f6ccd4b3bb028085d4c93d6cf7d165bf8709a468
Andrey Golikov <z-a-t-o@yandex.ru>
2022-01-07 10:52:19 +0300

    переформатировал код для лучшей читаемости

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from flask_admin.contrib.sqla import ModelView
 from flask_security import SQLAlchemyUserDatastore, Security, current_user
 from flask_babelex import Babel
 
-
 app = Flask(__name__)
 app.config.from_object(Configuration)
 data_base = SQLAlchemy(app)
@@ -20,6 +19,7 @@ migrate = Migrate(app, data_base)
 # Админка
 from models import *
 
+
 # Для того чтобы ограничить отображение админки созд. два класса от родительских ModelView и AdminIndexView
 # класс AdminView ограничивает вывод функционала админки
 class AdminView(ModelView):
@@ -28,7 +28,8 @@ class AdminView(ModelView):
 
     def inaccessible_callback(self, name, **kwargs):
         # параметр next перенаправляет на страницу куда мы хотели направиться будучи неавторизованным
-        return redirect( url_for('security.login', next=request.url) )
+        return redirect(url_for('security.login', next=request.url))
+
 
 # класс AdminPanel не даёт видеть админ панель будучи неавторизованным
 class AdminPanel(AdminIndexView):
@@ -36,26 +37,25 @@ class AdminPanel(AdminIndexView):
         return current_user.has_role('admin')
 
     def inaccessible_callback(self, name, **kwargs):
-        return redirect( url_for('security.login', next=request.url) )
-
+        return redirect(url_for('security.login', next=request.url))
 
 
 # создан экземпляр класса Admin
-admin = Admin(app, '@Msfo_stock_bot' , template_mode='bootstrap4', url='/', index_view=AdminPanel(name='Панель администратора'))
+admin = Admin(app, '@Msfo_stock_bot', template_mode='bootstrap4', url='/',
+              index_view=AdminPanel(name='Панель администратора'))
 # ModelView или расширеный от него, в нашем случае, AdminView - подхватывает классы б.д. из models.py
 # и реализуем модель 'C.R.U.D.' для управлением любыми данными из б.д.
 # параметром name можно назначить название кнопок на панели администратора.
 admin.add_view(AdminView(Records, data_base.session, name='Таблица МСФО/РСБУ'))
-admin.add_view(AdminView(User, data_base.session, name='Таблица пользователей'))
+admin.add_view(AdminView(User, data_base.session, name='Пользователи'))
 admin.add_view(AdminView(Role, data_base.session, name='Роли'))
 
 
 # локализация админки
 @babel.localeselector
 def get_locale():
-    # Put your logic here. Application can store locale in
-    # user profile, cookie, session, etc.
     return 'ru'
+
 
 # flask_security
 # созд. экземпляр класса SQLAlchemyUserDatastore, аргументы: б.д., классы User и Role из models.py
