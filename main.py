06253631e98e4e291c186e1604bbe53a3eeab399
Andrey Golikov <z-a-t-o@yandex.ru>
2022-01-09 18:36:08 +0300

    настройки local ip

--- main.py
+++ main.py
@@ -8,5 +8,6 @@ import view
 # регистрация блюпринта арг: экземпляр класса, путь до блюпринта
 app.register_blueprint(msfo_records, url_prefix='/msfo_records')
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port= 5000, debug=True)
