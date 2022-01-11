da3eb0763976fcd49a6d963f378f7c8fd1ade5b4
Andrey Golikov <z-a-t-o@yandex.ru>
2022-01-11 20:23:52 +0300

    Форма регистрации работает и готова на 80%

--- registration_form.py
+++ registration_form.py
@@ -1,14 +1,20 @@
-from wtforms import Form, StringField, EmailField, SelectField, PasswordField, DateField, SubmitField
-from wtforms.validators import DataRequired, Email, InputRequired, length
+from wtforms import StringField, EmailField, SelectField, PasswordField, DateField
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Email, InputRequired, EqualTo, length
 
 
-class Reg_form(Form):
+
+class Reg_form(FlaskForm):
     first_name = StringField('Имя: ', validators=[DataRequired(), length(max=20)])
     last_name = StringField('Фамилия: ', validators=[DataRequired(), length(max=20)])
     middle_name = StringField('Отчество: ', validators=[DataRequired(), length(max=20)])
-    email = EmailField('Почта: ', validators=[InputRequired('Введите адрес электронной почты.'),
-                Email('В этом поле нужно указать действующий адрес электронной почты'), length(max=100)])
-    password = PasswordField('Пароль: ', validators=[DataRequired(), length(max=255)])
-    gender = SelectField('Пол: ', choices=[('male', 'Мужской'), ('female', 'Женский')])
-    birthdate = DateField('Дата рождения')
+    email = EmailField('Почта: ', validators=[InputRequired(), Email(), length(max=100)])
 
+    password = PasswordField('Пароль: ', validators=[InputRequired(),
+                                EqualTo('confirm_pass', message='Пароли не совпадают. '),
+                                length(min=8, max=255, message='Пароль должен быть не менее 8 символов. ')])
+
+    confirm_pass = PasswordField('Повторите пароль: ', validators=[InputRequired()])
+
+    gender = SelectField('Пол: ', choices=[('Мужской', 'Мужской'), ('Женский', 'Женский')])
+    birthdate = DateField('Дата рождения', validators=[InputRequired()])
--- view.py
+++ view.py
@@ -1,8 +1,9 @@
 from app import app, data_base
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash
 from registration_form import Reg_form
 from models import User
-
+from app import user_datastore
+from flask_security.cli import hash_password
 
 
 
@@ -14,23 +15,29 @@ def index():
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        middle_name = request.form['middle_name']
-        email = request.form['email']
-        password = request.form['password']
-        gender = request.form['gender']
-        birthdate = request.form['birthdate']
+    form = Reg_form()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        middle_name = form.middle_name.data
+        email = form.email.data
+        password = hash_password(form.password.data)
+        gender = form.gender.data
+        birthdate = form.birthdate.data
         full_name = f'{last_name} {first_name} {middle_name}'
-        try:
-            user = User(full_name=full_name, email=email, password=password, gender=gender, birthdate=birthdate)
-            data_base.session.add(user)
-            data_base.session.commit()
-            return redirect(url_for('security.login'))
-        except:
-            data_base.session.rollback()
-            flash('Адрес электронной почты уже используется')
-            print('Ошибка записи в базу данных.')
-    forms = Reg_form()
-    return render_template('registration.html', forms=forms)
+
+        if not user_datastore.find_user(email=email):
+            try:
+                user = User(full_name=full_name, email=email, password=password, gender=gender, birthdate=birthdate)
+                data_base.session.add(user)
+                data_base.session.commit()
+                flash(f'{first_name} {middle_name}, Вы успешно зарегестрированы!', "success")
+                return redirect(url_for('security.login'))
+            except:
+                data_base.session.rollback()
+                print('Ошибка записи в базу данных.')
+        else:
+            flash(f'Адрес электронной почты {email} уже используется')
+    else:
+        pass
+    return render_template('registration.html', form=form)
