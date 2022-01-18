a39595e9864179b09382fa36955616f9a5ceabae
Andrey Golikov <z-a-t-o@yandex.ru>
2022-01-18 21:36:12 +0300

    кастомизация формы регистрации flask_security

--- registration_form.py
+++ registration_form.py
@@ -1,21 +1,27 @@
-from wtforms import StringField, EmailField, SelectField, PasswordField, DateField
-from flask_wtf import FlaskForm
-from wtforms.validators import Email, InputRequired, EqualTo, length, Regexp
+from wtforms import StringField, SelectField, PasswordField, DateField
+from wtforms.validators import InputRequired, EqualTo, length, Regexp
+from flask_security.forms import ConfirmRegisterForm
 
 
-class Reg_form(FlaskForm):
-    first_name = StringField('Имя: ', validators=[InputRequired(), length(max=20)])
-    last_name = StringField('Фамилия: ', validators=[InputRequired(), length(max=20)])
-    middle_name = StringField('Отчество: ', validators=[InputRequired(), length(max=20)])
-    email = EmailField('Почта: ', validators=[InputRequired(), Email(), length(max=100)])
 
+# Расширил класс ConfirmRegisterForm и переопределил валидацию пароля по современным стандартам
+class ExtendedRegisterForm(ConfirmRegisterForm):
     password = PasswordField('Пароль: ', validators=[InputRequired(),
-                        EqualTo('confirm_pass', message='Пароли не совпадают. '),
-                        length(min=8, max=255, message='Пароль должен быть не менее 8 символов. '),
-                        Regexp('(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%\-*#?&;:"\'])[A-Za-z\d@$!%\-*#?&;:"\']', message=
-                        'Пароль должен содержать минимум один символ в нижнем регистре, один в верхнем регистре, цифру, спецсимвол например: @$!%\-*#?&;:"\' ' )])
+                                                     EqualTo('password_confirm', message='Пароли не совпадают. '),
+                                                     length(min=8, max=255,
+                                                            message='Пароль должен быть не менее 8 символов. '),
+                                                     Regexp(
+                                                         '(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%\-*#?&;:"\'])[A-Za-z\d@$!%\-*#?&;:"\']',
+                                                         message=
+                                                         'Пароль должен содержать минимум один символ в нижнем регистре, '
+                                                         'один в верхнем регистре, цифру, спецсимвол например: @$!%\-*#?&;:"\' ')])
 
-    confirm_pass = PasswordField('Повторите пароль: ', validators=[InputRequired()])
-
+    password_confirm = PasswordField('Повторите пароль: ', validators=[InputRequired()])
+    first_name = StringField('Имя: ', validators=[InputRequired(), length(max=50)])
+    last_name = StringField('Фамилия: ', validators=[InputRequired(), length(max=50)])
+    middle_name = StringField('Отчество: ', validators=[InputRequired(), length(max=50)])
     gender = SelectField('Пол: ', choices=[('Мужской', 'Мужской'), ('Женский', 'Женский')])
     birthdate = DateField('Дата рождения', validators=[InputRequired()])
+
+
+
